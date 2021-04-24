6b22c73d5d1abb90a29f5fa9879647b570452155
Bhakti Dilkhush <bhaktidilkhush.2000@gmail.com>
2021-04-24 17:04:42 +0530

    committ 1

--- Codevilla/script.js
+++ Codevilla/script.js
@@ -1,3 +1,26 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
+
+  var attempt = 3; // Variable to count number of attempts.
+  // Below function Executes on click of login button.
+  function validate(){
+  var username = document.getElementById("uname").value;
+  var password = document.getElementById("pass").value;
+  if ( username == "Bhakti" && password == "123"){
+  alert ("Login successfully");
+  window.location = "JavascriptCourse.html"; // Redirecting to other page.
+  return false;
+  }
+  else{
+  attempt --;// Decrementing by one.
+  alert("You have left "+attempt+" attempt;");
+  // Disabling fields after 3 attempts.
+  if( attempt == 0){
+  document.getElementById("uname").disabled = true;
+  document.getElementById("pass").disabled = true;
+  document.getElementById("submit").disabled = true;
+  return false;
+  }
+  }
+  }
